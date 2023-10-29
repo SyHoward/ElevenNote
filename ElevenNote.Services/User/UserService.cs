623342908d2a8026093de9ce078d40af5dcefc16
Sy Howard <symanthacrh@gmail.com>
2023-10-28 21:48:13 -0400

    11.02

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -12,8 +12,8 @@ public class UserService : IUserService
     private readonly SignInManager<UserEntity> _signInManager;
 
     public UserService(ApplicationDbContext context,
-                       UserManager<UserEntity> userManager,
-                       SignInManager<UserEntity> signInManager)
+                        UserManager<UserEntity> userManager,
+                        SignInManager<UserEntity> signInManager)
     {
         _context = context;
         _userManager = userManager;
@@ -40,12 +40,30 @@ public class UserService : IUserService
             UserName = model.UserName,
             DateCreated = DateTime.Now
         };
-        
+
         IdentityResult registerResult = await _userManager.CreateAsync(entity, model.Password);
 
         return registerResult.Succeeded;
     }
 
+    public async Task<UserDetail?> GetUserByIdAsync(int userId)
+    {
+        UserEntity? entity = await _context.Users.FindAsync(userId);
+        if (entity is null)
+            return null;
+
+            UserDetail detail = new()
+            {
+                Id = entity.Id,
+                Email = entity.Email,
+                UserName = entity.UserName!,
+                FirstName = entity.FirstName!,
+                LastName = entity.LastName,
+                DateCreated = entity.DateCreated
+            };
+
+            return detail;
+    }
     private async Task<bool> CheckUserNameAvailability(string userName)
     {
         UserEntity? existingUser = await _userManager.FindByNameAsync(userName);
--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ElevenNote.Models.Responses;
+using ElevenNote.Models.User;
 using ElevenNote.Services.User;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebAPI.Controllers;
@@ -16,6 +13,24 @@ public class UserController : ControllerBase
     public UserController(IUserService userService)
     {
         _userService = userService;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var registerResult = await _userService.RegisterUserAsync(model);
+        if (registerResult)
+        {
+            TextResponse response = new("User was registered.");
+            return Ok(response);
+        }
+
+        return BadRequest(new TextResponse("User could not be registered."));
     }
 }
 
