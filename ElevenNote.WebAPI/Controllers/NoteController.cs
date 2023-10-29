1dbdad9ee93adcf462ad3d70896a958677222d46
Sy Howard <symanthacrh@gmail.com>
2023-10-29 19:41:07 -0400

    17.03

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -1,5 +1,6 @@
 using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebAPI.Controllers;
@@ -10,10 +11,17 @@ namespace ElevenNote.WebAPI.Controllers;
 public class NoteController : ControllerBase
 {
     private readonly INoteService _noteService;
-    
+
     public NoteController(INoteService noteService)
     {
         _noteService = noteService;
     }
-}
 
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllNotes()
+    {
+        var notes = await _noteService.GetAllNotesAsync();
+        return Ok(notes);
+    }
+}
