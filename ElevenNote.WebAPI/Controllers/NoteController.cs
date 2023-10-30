aba07e9e9ee413e242880dde33095cc87c71d3f7
Sy Howard <symanthacrh@gmail.com>
2023-10-30 12:25:33 -0400

    18.03

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -1,6 +1,7 @@
+using ElevenNote.Models.Note;
+using ElevenNote.Models.Responses;
 using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebAPI.Controllers;
@@ -17,6 +18,18 @@ public class NoteController : ControllerBase
         _noteService = noteService;
     }
 
+    [HttpPost]
+    public async Task<IActionResult> CreateNote([FromBody] NoteCreate request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var response = await _noteService.CreateNoteAsync(request);
+        if (response is not null)
+            return Ok(response);
+
+        return BadRequest(new TextResponse("Could not create note."));
+    }
 
     [HttpGet]
     public async Task<IActionResult> GetAllNotes()
